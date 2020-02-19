7eb9195838288cbced168245d31e2497c4c6963b
Luna Campos <lunacampos@Lunas-MacBook-Pro.local>
2020-02-19 15:58:33 -0800

    anotha commit

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,11 +8,38 @@ const path = require('path');
 // });
 
 // Create and write to file
-fs.writeFile(
-  path.join(__dirname, '/test', 'hello.txt'),
-  'I love Node.js',
+// fs.writeFile(
+//   path.join(__dirname, '/test', 'hello.txt'),
+//   'Hello Wogfgrld!',
+//   (err)=>{
+//     if(err) throw err;
+//     console.log('File written to...');
+//
+//     //File Append
+//     fs.appendFile(
+//       path.join(__dirname, '/test', 'hello.txt'),
+//       ' I love Nofgfde.js',
+//       (err)=>{
+//         if(err) throw err;
+//         console.log('File written to...');
+//       }
+//     );
+//
+//   }
+// );
+
+
+//  Read file
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//   if(err) throw err;
+//   console.log(data);
+// });
+
+// Rename file
+fs.rename(path.join(__dirname, 'test', 'hello.txt'),
+  path.join(__dirname, 'test', 'helloworld.txt'),
   (err)=>{
     if(err) throw err;
-    console.log('File written to...');
+    console.log('File renamed...');
   }
-);
+)
